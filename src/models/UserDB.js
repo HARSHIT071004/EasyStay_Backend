6776b91494ccc81f9942811e4e77548f815f96aa
harshit sharma <harshsharmavatasa@gmail.com>
2025-04-18 11:48:09 -0700

    first commit

--- src/models/UserDB.js
+++ src/models/UserDB.js
@@ -1,12 +1,14 @@
 var mysql = require('mysql2');
 
 var pool = mysql.createPool({
-    connectionLimit: 100,
     port: 3306,
     host:'sql12.freesqldatabase.com',
     user:'sql12774041',
     password:'JlAW99amN1',
     database:'sql12774041',
+    waitForConnections: true, 
+    connectionLimit: 10,   
+    queueLimit: 0
     
 });
 
